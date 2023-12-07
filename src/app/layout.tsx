cd5abeb0c176a544960e016f212ca1523f8d071d
vdeva <va.devmail@protonmail.com>
2023-12-06 21:02:21 -0800

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import { Footer } from "@/components/footer";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Libre_Baskerville } from "next/font/google";
-import { Open_Sans } from "next/font/google";
-import Link from "next/link";
+import { Libre_Baskerville, Open_Sans } from "next/font/google";
 
 const lb = Libre_Baskerville({
   subsets: ["latin"],
@@ -38,6 +37,7 @@ export default function RootLayout({
       <body className={`${lb.variable} ${opensans.variable}`}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
